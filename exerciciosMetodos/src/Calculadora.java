581022788cf31953975085ef5e8d0ad1b010489f
JoaoFilho1234 <joaofilho.jbf28@gmail.com>
2022-01-24 17:06:35 -0300

    metodos

---
+++ exerciciosMetodos/src/Calculadora.java
@@ -0,0 +1,27 @@
+public class Calculadora {
+
+    public static void soma(double a, double b) {
+        double resultado = a + b;
+        System.out.println("A soma de "+ a + " mais " + b + " é igual "+resultado );
+    }
+
+    public static void subtracao(double a, double b) {
+        double resultado = a - b;
+        System.out.println("O resultado da subtração de "+a+ " mais "+b+ " é igual "+resultado);
+    }
+
+    public static void multiplicação(double a, double b) {
+        double resultado = a * b;
+        System.out.println("O resultado da multiplicação de "+a+ " por "+b+" é "+resultado );
+    }
+
+    public static void divisao(double a, double b) {
+        double resultado = a / b;
+        System.out.println("O resultado da divisão de "+a+" por "+b+" é "+resultado);
+    }
+
+
+
+
+
+}
